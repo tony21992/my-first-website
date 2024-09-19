5239374ef0e6f986da1dc92c8eb7437a70ca09f3
tony21992 <tony2working@gmail.com>
2024-09-19 23:41:42 +0800

    feat: add image ai function

--- app/image-generator/page.js
+++ app/image-generator/page.js
@@ -1,23 +1,49 @@
 "use client";
 
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import axios from "axios";
 import { faImage } from "@fortawesome/free-solid-svg-icons"
 import CurrentFileIndicator from "@/components/CurrentFileIndicator";
 import PageHeader from "@/components/PageHeader";
 import GeneratorButton from "@/components/GenerateButton";
+import ImageGenCard from "@/components/ImageGenCard";
+import ImageGenPlaceholder from "@/components/ImageGenPlaceholder";
 
 export default function ImgGen() {
     const [userInput, setUserInput] = useState("");
+    const [carList, setCardList] = useState ([]);
     // 是否在等待回應
     const [isWaiting, setIsWaiting] = useState(false);
+
+    useEffect(() => {
+        axios.get("/api/image-ai")
+        .then(res => {
+            const newCardList = res.data;
+            setCardList(newCardList);
+        })
+        .catch(err => {
+
+        })
+    }, []);
 
     function submitHandler(e) {
         e.preventDefault();
         console.log("User Input: ", userInput);
         const body = { userInput };
         console.log("body:", body);
+        setUserInput("");
+        setIsWaiting(true)
         // TODO: 將body POST到 /api/image-ai { userInput: "" }
+        axios.post("/api/image-ai", body)
+        .then(res => {
+            setIsWaiting(false);
+            const card = res.data;
+            setCardList([card, ...carList]);
+        })
+        .catch(err => {
+            setIsWaiting(false);
+            alert ("錯誤");
+        });
 
 
     }
@@ -50,6 +76,18 @@ export default function ImgGen() {
             <section>
                 <div className="container mx-auto">
                     {/* TODO: 顯示AI輸出結果 */}
+                    <div className="grid grid-cols-4 gap-2 md;grid-cols-3 lg:grrid-cols-4 gap-4 py-4">
+                        {isWaiting ? <ImageGenPlaceholder />: null }
+                        {carList.map(card => {
+                            const { imageURL, prompt, createdAt} = card;
+                            return <ImageGenCard 
+                                imageURL= {imageURL}
+                                prompt={prompt}
+                                key={createdAt}
+                            />
+                        })}
+                    
+                    </div>
 
                 </div>
             </section>
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    image: {
+        remotePatterns : [
+            {
+                protocol: 'https',
+                hostname: '',
+                port: '',
+                pathname: '/**',
+            },
+        ],
+    },
+};
 
 export default nextConfig;
